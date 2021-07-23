d14b3126558c497599c0eaeab869b713fd8a7124
Gumarov Lazar <gumarov.lazar@gmail.com>
2021-07-23 14:43:18 +0500

    feat: add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return <nav className="navbar">
+        <ul className="menu">
+          <li><a href="#" className="menu-link">Home</a></li>
+          <li><a href="#" className="menu-link">Pictures</a></li>
+          <li><a href="#" className="menu-link">Awards</a></li>
+          <li><a href="#" className="menu-link">Volunteering</a></li>
+          <li><a href="#" className="menu-link">Contacts</a></li>
+        </ul>
+      </nav>   
+}
+
+export default Navbar;
