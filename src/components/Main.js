1d24ca0a3a42c780660ef5cfc8aae40d81fc75ee
Gumarov Lazar <gumarov.lazar@gmail.com>
2021-07-26 11:54:41 +0500

    feat: add router

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import Picture from "./Picture";
+
+const Main = () => {
+  return (
+    <div>
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+      <Picture />
+    </div>
+  );
+};
+
+export default Main;
